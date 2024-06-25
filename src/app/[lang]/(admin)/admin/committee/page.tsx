b877407c62d9dc349164091f5b5d3c495fb857cc
Roshan Paudel <rozanpoudel@gmail.com>
2024-06-25 11:19:37 +0545

    🐞 FIX: admin new page

--- src/app/[lang]/(admin)/admin/committee/page.tsx
+++ src/app/[lang]/(admin)/admin/committee/page.tsx
@@ -7,6 +7,20 @@ const links = [
   { title: "Central Comittee", to: "/central" },
   { title: "Departmental Heads", to: "/departmental" },
   { title: "Provincial Comittee", to: "/provincial" },
+  { title: "Co Society", to: "/co-society" },
+];
+
+const countries = [
+  { title: "America", to: "/america" },
+  { title: "India", to: "/india" },
+  { title: "China", to: "/china" },
+  { title: "United Kingdom", to: "/united-kingdom" },
+  { title: "Europe", to: "/europe" },
+  { title: "Japan", to: "/japan" },
+  { title: "France", to: "/france" },
+  { title: "Canada", to: "/canada" },
+  { title: "Australia", to: "/australia" },
+  { title: "Others", to: "/others" },
 ];
 
 const page = () => {
@@ -15,6 +29,21 @@ const page = () => {
       <H2 className="border-b pb-2">All Committee</H2>
       <ul className="space-y-2">
         {links.map((link, idx) => (
+          <li>
+            <Link
+              className="text-blue-600 underline-offset-4 hover:underline"
+              key={idx}
+              href={`/admin/committee/${link.to}`}
+            >
+              {link.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <h2 className="pb-4 pt-10 text-xl font-semibold">Foreign Departments</h2>
+      <ul className="space-y-2">
+        {countries.map((link, idx) => (
           <li>
             <Link
               className="text-blue-600 underline-offset-4 hover:underline"
